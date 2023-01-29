829a84aed0fdd5c7b5cbfb9c1b3e331e9ffc4923
VincentZhang <zhangleqiqiulelin@outlook.com>
2023-01-29 21:48:07 +0800

    Fixed a bug:when the number is negative,the result is 0.

--- revernumber.cpp
+++ revernumber.cpp
@@ -3,10 +3,20 @@
 
 int main(int argc, char* argv[])
 {
-    int a = 123450000;
+    int a = -12345;
     int number_a = a;
     int result = 0;
     int digit_a = 0;
+    while(a<0)
+    {
+        number_a = number_a / 10;
+        number_a = floor(number_a);
+        number_a = number_a * 10;
+        int digit_a = a - number_a;
+        result = result * 10 + digit_a;
+        a = a/10;
+        number_a = a;
+    }
     while(a>0)
     {
         number_a = number_a / 10;
